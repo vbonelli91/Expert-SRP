f02cbbbe344dd94ebcae86877641a7cadb81a247
vbonelli91 <69222838+vbonelli91@users.noreply.github.com>
2020-09-01 20:17:27 -0300

    Ejercicio1

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -4,6 +4,11 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
+        /*
+Cumple con el Expert ya que la información necesaria para poder cumplir con la responsabilidad. 
+En cuanto al SRP no se cumple ya que la responsabilidad no se encuentra completamente encapsulada por la clase. 
+
+*/
         public Alfajor(double precioMasa, double precioDulce)
         {
             this.PrecioDulce = precioDulce;
