c1cb96cdfdb3e2607c72c7b08cf549404a1c6cc8
vbonelli91 <69222838+vbonelli91@users.noreply.github.com>
2020-09-08 20:04:21 -0300

    Add files via upload

---
+++ Ejercicio 4/Program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PII_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "assets\board.txt";
+            bool[,] board= LeerArchivo.lecturaArchivo(url);
+            
+        }
+    }
+}
